377341af9c85029085bed8b1aab0e43f97cbbc74
dikiypes <Darkorbitint1@yandex.ru>
2024-02-27 20:08:40 +0300

    2222

--- lms/config/settings.py
+++ lms/config/settings.py
@@ -76,13 +76,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my_db',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -124,3 +134,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
--- lms/users/models.py
+++ lms/users/models.py
@@ -13,9 +13,12 @@ class Payment(models.Model):
 
 
 class User(AbstractUser):
+    username = None
     groups = models.ManyToManyField(Group, related_name='custom_user_set')
     user_permissions = models.ManyToManyField(Permission, related_name='custom_user_set_permissions')
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=15)
     city = models.CharField(max_length=255)
     avatar = models.ImageField(upload_to='avatars/')
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
